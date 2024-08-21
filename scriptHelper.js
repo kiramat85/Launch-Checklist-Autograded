cecaf45e3a83cf70c1cdea8210a1901b4a88d8d2
Hamida Abdul Zahir <hamidaabdulz85@gmail.com>
2024-08-21 10:30:17 -0400

    working code

--- scriptHelper.js
+++ scriptHelper.js
@@ -4,21 +4,28 @@ require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
-    /*
-                 <h2>Mission Destination</h2>
-                 <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
-                     <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
-                 </ol>
-                 <img src="">
-    */
+    const div = document.getElementById("missionTarget")
+    div.innerHTML = `
+    <h2>Mission Destination</h2>
+    <ol>
+        <li>Name:${arr[i].name}  </li>
+        <li>Diameter:${arr[i].diameter} </li>
+        <li>Star: ${arr[i].star}</li>
+        <li>Distance from Earth: ${arr[i].distance} </li>
+        <li>Number of Moons: ${arr[i].moons}</li>
+    </ol>
+    <img src=${arr[i].image}>`
+ 
  }
  
  function validateInput(testInput) {
-    
+    if (testInput == ""){
+        return "Empty";
+    }else  if( isNaN(testInput)){
+        return "Not a Number";
+    }else if( !isNaN(testInput)){
+        return "Is a Number"
+    }
  }
  
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
